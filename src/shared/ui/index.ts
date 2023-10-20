875cfbe7da55ea95e0687844dc8cf9db8c94b24c
3str-webdev <3str.webdev@gmail.com>
2023-10-20 16:12:03 +0300

    feat: add ui entry points

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,3 @@
+export { UILoader, type UILoaderProps } from "./ui-loader";
+export { UIPageLoader, type UIPageLoaderProps } from "./ui-page-loader";
+export { UIVideoPlayer, type UIVideoPlayerProps } from "./ui-video-player";
