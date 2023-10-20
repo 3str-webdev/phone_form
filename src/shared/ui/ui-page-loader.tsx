48ca3b615b40c32c6165f7e889ab2be0002ac47f
3str-webdev <3str.webdev@gmail.com>
2023-10-20 16:11:19 +0300

    feat: add ui-page-loader component

---
+++ src/shared/ui/ui-page-loader.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+import { HTMLAttributes } from "react";
+import { UILoader } from "./ui-loader";
+
+export type UIPageLoaderProps = HTMLAttributes<HTMLDivElement>;
+
+export function UIPageLoader({ className, ...props }: UIPageLoaderProps) {
+  return (
+    <div
+      {...props}
+      className={clsx(
+        className,
+        "w-full h-full flex items-center justify-center"
+      )}
+    >
+      <UILoader className="w-14 h-14 text-sky-400" />
+    </div>
+  );
+}
