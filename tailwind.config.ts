33d3dd0e790f32809ef63922eceb8439a8708998
3str-webdev <3str.webdev@gmail.com>
2023-10-20 18:02:19 +0300

    feat: add design sistem to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,31 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    colors: {
+      main: {
+        1: "#86D3F4",
+        2: "#000000",
+      },
+      subline: {
+        1: "#565656",
+      },
+      error: {
+        1: "#EA0000",
+      },
+      disabled: {
+        1: "#4E4E4E",
+      },
+      transparent: "rgba(0, 0, 0, 0)",
+    },
+
+    fontSize: {
+      header: "26px",
+      text: "16px",
+      subtext: "14px",
+      telnumber: "32px",
+    },
+  },
   plugins: [],
 };
 export default config;
