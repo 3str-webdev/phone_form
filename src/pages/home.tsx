911e7d166a869079f6f57c4237c3ab3a2499a3bb
3str-webdev <3str.webdev@gmail.com>
2023-10-20 16:14:27 +0300

    feat: add using layout and bg video

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,30 @@
+import { UIPageLoader, UIVideoPlayer } from "@/shared/ui";
+import { VideoLayout } from "@/widgets/bg-video";
+import { useState } from "react";
+
 export function HomePage() {
-  return <></>;
+  const [isLoadingPage, setIsLoadingPage] = useState<boolean>(true);
+
+  const videoCLassName = isLoadingPage ? "hidden" : "";
+
+  const handleVideoLoadComplete = () => {
+    setIsLoadingPage(false);
+  };
+
+  return (
+    <VideoLayout>
+      {isLoadingPage && <UIPageLoader />}
+
+      <UIVideoPlayer
+        className={videoCLassName}
+        src="/bg-video.mp4"
+        autoPlay
+        muted
+        loop
+        preload="metadata"
+        onLoadComplete={handleVideoLoadComplete}
+        onCanPlay={handleVideoLoadComplete}
+      />
+    </VideoLayout>
+  );
 }
