1abe481ace1b80939c35f137da313b38a533e10d
3str-webdev <3str.webdev@gmail.com>
2023-10-24 15:19:35 +0300

    feat: add app layout

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,31 @@
+import { useFocusableStore } from "@/features/focus-navigate";
+import { ROUTES } from "@/shared/constants/routes";
+import { useVideoStore } from "@/shared/store/use-video-store";
+import { VideoLayout } from "@/widgets/bg-video";
+import { BgVideo } from "@/widgets/bg-video/";
 import type { AppProps } from "next/app";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const pathname = usePathname();
+  const { play, pause } = useVideoStore((state) => ({
+    play: state.play,
+    pause: state.pause,
+  }));
+
+  useEffect(() => {
+    if (pathname !== ROUTES.HOME) {
+      pause();
+    } else {
+      play();
+    }
+  }, [pathname, pause, play]);
+
+  return (
+    <VideoLayout>
+      <BgVideo />
+      <Component {...pageProps} />
+    </VideoLayout>
+  );
 }
