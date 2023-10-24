99052ef212f9f98953708957e5a8eea4d7fe3e02
3str-webdev <3str.webdev@gmail.com>
2023-10-24 15:06:27 +0300

    feat: add prove personal data widget

---
+++ src/widgets/prove-personal-data/ui/prove-personal-data.tsx
@@ -0,0 +1,49 @@
+import {
+  RegisterFocusableComponent,
+  RegisterFocusableComponentProps,
+} from "@/features/focus-navigate";
+import { UICheckbox, UICheckboxProps } from "@/shared/ui";
+import clsx from "clsx";
+import { forwardRef } from "react";
+
+type ProvePersonalDataProps = UICheckboxProps;
+
+export const ProvePersonalDataComponent = forwardRef<
+  HTMLLabelElement,
+  ProvePersonalDataProps
+>(function Prove({ children, ...props }, ref) {
+  return (
+    <label
+      ref={ref}
+      onClick={() => props.onChange(!props.checked)}
+      className={clsx(
+        "flex items-center gap-[10px] cursor-pointer outline-none",
+        "focus:outline-2 focus:outline-main-2"
+      )}
+      tabIndex={0}
+    >
+      <div className="px-[10px] py-[6px]">
+        <UICheckbox {...props} />
+      </div>
+      <p className="text-subtext text-subline-1">{children}</p>
+    </label>
+  );
+});
+
+export function ProvePersonalData({
+  focusId,
+  component = ProvePersonalDataComponent,
+  moves,
+  props,
+  ...otherProps
+}: RegisterFocusableComponentProps<ProvePersonalDataProps>) {
+  return (
+    <RegisterFocusableComponent
+      {...otherProps}
+      component={component}
+      focusId={focusId}
+      moves={moves}
+      props={props}
+    />
+  );
+}
