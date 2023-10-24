9ad35d45b141e73756eba1b3143fbff2121d9e20
3str-webdev <3str.webdev@gmail.com>
2023-10-24 16:45:34 +0300

    feat: setting env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+require("dotenv").config();
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    PHONE_VALIDATE_API_KEY: process.env.PHONE_VALIDATE_API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
