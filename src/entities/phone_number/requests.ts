aec2e9a43e25c9be29c0e4e99359e9d72eaf326d
3str-webdev <3str.webdev@gmail.com>
2023-10-24 16:46:13 +0300

    feat: add validate phone query

---
+++ src/entities/phone_number/requests.ts
@@ -0,0 +1,25 @@
+import { Country } from "@/features/enter-phone-number";
+import axios from "axios";
+
+export const validPhone = async (
+  phone: string,
+  country: Country
+): Promise<boolean> => {
+  try {
+    const url = "http://apilayer.net/api/validate";
+    const response = await axios<{ valid: boolean }>(url, {
+      params: {
+        access_key: process.env.PHONE_VALIDATE_API_KEY,
+        number: phone,
+        country_code: country,
+      },
+    });
+
+    return response.data.valid;
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(`Ошибка при проверке номера: ${err.message}`);
+    }
+    return false;
+  }
+};
