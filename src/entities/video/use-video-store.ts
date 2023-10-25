e0c6b2e7d2f28c84e6c53ff7baa2360414c556db
3str-webdev <3str.webdev@gmail.com>
2023-10-25 11:56:43 +0300

    update: extract video store in video entity

--- src/entities/video/use-video-store.ts
+++ src/entities/video/use-video-store.ts
@@ -2,16 +2,18 @@ import { create } from "zustand";
 
 type UseVideoStore = {
   isPlaying: boolean;
-  currentTime: number;
   play: () => void;
   pause: () => void;
+
+  currentTime: number;
   setCurrentTime: (time: number) => void;
 };
 
 export const useVideoStore = create<UseVideoStore>((set) => ({
   isPlaying: true,
-  currentTime: 0,
   play: () => set({ isPlaying: true }),
   pause: () => set({ isPlaying: false }),
+
+  currentTime: 0,
   setCurrentTime: (time) => set({ currentTime: time }),
 }));
